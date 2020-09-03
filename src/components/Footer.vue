8811a9bffa53abb7eab74f91dfe454fcb78ac656
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2020-09-03 19:58:21 +0700

    convert html template to vue components

---
+++ src/components/Footer.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <!-- footer_start  -->
+    <footer class="footer">
+      <div class="footer_top">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4 ">
+              <div class="footer_widget">
+                <div class="footer_logo">
+                  <a href="#">
+                    <img src="img/footer_logo.png" alt="">
+                  </a>
+                </div>
+                <p class="address_text">Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit, sed do <br>
+                  eiusmod tempor incididunt ut labore.
+                </p>
+                <div class="socail_links">
+                  <ul>
+                    <li>
+                      <a href="#">
+                        <i class="ti-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="ti-twitter-alt"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="fa fa-dribbble"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+              <div class="footer_widget">
+                <h3 class="footer_title">
+                  Contacts
+                </h3>
+                <div class="contacts">
+                  <p>+2(305) 587-3407 <br>
+                    info@loveuscharity.com <br>
+                    Flat 20, Reynolds Neck, North
+                    Helenaville, FV77 8WS
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copy-right_text">
+        <div class="container">
+          <div class="row">
+            <div class="bordered_1px "></div>
+            <div class="col-xl-12">
+              <div class="copy_right text-center">
+                <p>Copyright &copy; 2020 All rights reserved | Develop <i class="ti-heart" aria-hidden="true"></i> by <a
+                    href="www.github.com/Zainal21" target="_blank">Muhamad Zainal Arifin</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- footer_end  -->
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Footer'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
-   <HelloWorld msg="Halo Semua"/>
+    <Header />
+    <Slider/>
+    <Reason/>
+    <Populer/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Slider from '@/components/Slider.vue'
+import Reason from '@/components/Reason.vue'
+import Populer from '@/components/Populer.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'Home',
   components:{
-    HelloWorld
+    Header,
+    Slider,
+    Reason,
+    Populer,
+    Footer
   }
 }
 </script>
